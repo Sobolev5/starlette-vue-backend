b3e33459b39542ff6985ce080bdfe45ea3a6b015
Andrey Sobolev <email.asobolev@gmail.com>
2020-06-07 09:01:42 +0300

    Sobolev--make-channels

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -1,5 +1,9 @@
 from starlette.routing import Route
+from starlette.routing import WebSocketRoute
 
+from .views import Chat
+from .views import ChatTest1
+from .views import ChatTest2
 from .views import hello
 from .views import main
 from .views import private
@@ -9,4 +13,7 @@ routes = [
     Route("/", endpoint=main, name="main__main"),
     Route("/hello/{word:str}/", endpoint=hello, name="main__hello"),
     Route("/private/", endpoint=private, name="main__private", methods=["POST", "OPTIONS"]),
+    Route("/chat1/", endpoint=ChatTest1, name="main__chat1"),
+    Route("/chat2/", endpoint=ChatTest2, name="main__chat2"),
+    WebSocketRoute("/chat_ws", Chat),
 ]
