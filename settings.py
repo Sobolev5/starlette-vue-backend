bbba329785fd14d28ab058e30f2ba4427ed0a159
EvgeniyMarkulchak <evgeniy.markulchak@gmail.com>
2020-06-18 21:22:20 +0300

    Markulchak--Mailer

--- settings.py
+++ settings.py
@@ -12,3 +12,8 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=CommaSeparatedStrings)
 JWT_PREFIX = config("JWT_PREFIX", cast=str)
 JWT_ALGORITHM = config("JWT_ALGORITHM", cast=str)
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+
+# Mailer settings
+MAILJET_API_KEY = config("MAILJET_API_KEY", cast=str)
+MAILJET_API_SECRET = config("MAILJET_API_SECRET", cast=str)
+USE_MAILER = config("USE_MAILER", cast=bool, default=False)
